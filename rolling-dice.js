985f12f802daeb2a5707ae968fead79c2bb7683e
Mark Mckittrick <mckittrickmark@gmail.com>
2018-09-25 14:37:51 +0000

    try two

---
+++ rolling-dice.js
@@ -0,0 +1,14 @@
+var args = process.argv.slice(2)
+var resultsContainer = []
+var outputString = "Rolled " + args + " dice: "
+
+function outputDice(numberOfDice) {
+  for (i = 0 ; i < numberOfDice ; i++) {
+    outputString += Math.ceil(Math.random() * 6) + ", ";
+  }
+  return outputString.slice(0,-2);
+};
+
+
+console.log(outputDice(args));
+
